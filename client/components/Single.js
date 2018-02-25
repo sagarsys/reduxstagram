dfb88f5ac79e4a14a7596b56a612167652c799ac
sagarsys <sagar_sys@live.com>
2018-02-26 03:54:42 +0400

    - Adding comments + reducers

--- client/components/Single.js
+++ client/components/Single.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import Photo from './Photo';
+import Comments from './Comments';
 
 class Single extends React.Component {
     render() {
+        // post id from url
+        const { postId } = this.props.params;
+        // index of post
+        const i = this.props.posts.findIndex((post) => post.code === postId);
+        // get the post
+        const post = this.props.posts[i];
+        // get the comments
+        const postComments = this.props.comments[postId] || [];
+        
         return (
             <div className="single-photo">
-                Single!!
+                <Photo i={i} post={post} {...this.props} />
+                <Comments postComments={postComments} {...this.props} />
             </div>
         )
     }
--- client/reducers/comments.js
+++ client/reducers/comments.js
@@ -1,5 +1,33 @@
+const postComment = (state = [], action) => {
+    console.log(action)
+    switch (action.type) {
+        case 'ADD_COMMENT':
+            // return updated state with new comment
+            return [...state, {
+                user: action.author,
+                text: action.comment
+            }];
+        case 'REMOVE_COMMENT':
+            // return updated state with deleted comment 
+            return [
+                ...state.slice(0, action.index),
+                ...state.slice(action.index + 1)
+            ];
+        default:
+            return state;
+    }
+};
+
 const comments = (state = [], action) => {
-    console.log(state, action);
+    if (typeof action.postId !== 'undefined') {
+        return {
+            // take the current state
+            ...state,
+            // overwrite this post with a new one
+            [action.postId]: postComment(state[action.postId], action)
+        }
+    }
+
     return state;
 };
 
